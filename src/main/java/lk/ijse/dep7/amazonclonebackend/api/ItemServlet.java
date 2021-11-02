d836f6b3d9663f053d6430b2e3c3ebdc452c8f25
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-02 13:16:14 +0530

    Update the doGet() method of ItemServlet to support query parameter 'code'

--- src/main/java/lk/ijse/dep7/amazonclonebackend/api/ItemServlet.java
+++ src/main/java/lk/ijse/dep7/amazonclonebackend/api/ItemServlet.java
@@ -26,13 +26,30 @@ public class ItemServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+        // GET http://localhost:8080/amazon/items           <- All items
+        // GET http://localhost:8080/amazon/items?code=I001 <- Item I001
+
         try (Connection connection = dataSource.getConnection()) {
-            ItemService itemService = new ItemService(connection);
-            List<ItemDTO> items = itemService.getAllItems();
             Jsonb jsonb = JsonbBuilder.create();
+            ItemService itemService = new ItemService(connection);
+            String code = request.getParameter("code");
 
             response.setContentType("application/json");
-            response.getWriter().println(jsonb.toJson(items));
+
+            if (code != null){
+                ItemDTO item = itemService.getItem(code);
+
+                if (item != null){
+                    response.getWriter().println(jsonb.toJson(item));
+                }else{
+                    response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                }
+            }else{
+                List<ItemDTO> items = itemService.getAllItems();
+                response.getWriter().println(jsonb.toJson(items));
+            }
+
+
         } catch (SQLException ex) {
             ex.printStackTrace();
             throw new RuntimeException("Failed to obtain a new connection", ex);
