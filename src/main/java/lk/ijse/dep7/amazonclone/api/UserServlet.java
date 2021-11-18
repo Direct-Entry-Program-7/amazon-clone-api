11a0e68bb7d78b544f70e1a5f6eb2dcfece951f7
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-18 19:45:58 +0530

    Implement sign-in

--- src/main/java/lk/ijse/dep7/amazonclone/api/UserServlet.java
+++ src/main/java/lk/ijse/dep7/amazonclone/api/UserServlet.java
@@ -4,9 +4,11 @@ import jakarta.annotation.Resource;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import jakarta.json.bind.JsonbException;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lk.ijse.dep7.amazonclone.dto.UserDTO;
 import lk.ijse.dep7.amazonclone.service.UserService;
 
@@ -15,10 +17,10 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-@WebServlet(name = "UserServlet", value = "/users")
+@WebServlet(name = "UserServlet", urlPatterns = {"/users", "/login"})
 public class UserServlet extends HttpServlet {
 
-    @Resource(name= "java:comp/env/jdbc/amazon")
+    @Resource(name = "java:comp/env/jdbc/amazon")
     private DataSource dataSource;
 
     @Override
@@ -28,41 +30,80 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getContentType() == null || !request.getContentType().equalsIgnoreCase("application/json")){
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid content type");
-            return;
-        }
 
-        try (Connection connection = dataSource.getConnection()){
-            Jsonb jsonb = JsonbBuilder.create();
-            UserDTO user = jsonb.fromJson(request.getReader(), UserDTO.class);
+        switch (request.getServletPath()) {
 
-            if (user == null){
-                throw new JsonbException("Invalid user");
-            }else if (user.getUserId() == null || user.getUserId().trim().isEmpty()){
-                throw new JsonbException("User id can't be empty");
-            }else if (user.getUserId().trim().length() < 5){
-                throw new JsonbException("User id should be at least 5 chars long");
-            }else if (user.getName() == null || user.getName().trim().isEmpty()){
-                throw new JsonbException("User's name can't be empty");
-            }else if (!user.getName().trim().matches("[A-Za-z ]+")){
-                throw new JsonbException("Invalid name");
-            }else if (user.getPassword() == null || user.getPassword().trim().isEmpty()){
-                throw new JsonbException("Password can't be empty");
-            }else if (user.getPassword().trim().length() < 5){
-                throw new JsonbException("Password should be at lest 5 chars long");
-            }else if (user.getUserId().contains(":")){
-                throw new JsonbException("User id can't contain colon");
-            }
+            case "/users":
+                if (request.getContentType() == null || !request.getContentType().equalsIgnoreCase("application/json")) {
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid content type");
+                    return;
+                }
 
-            UserService userService = new UserService(connection);
-            userService.saveUser(user);
-            response.setStatus(HttpServletResponse.SC_CREATED);
+                try (Connection connection = dataSource.getConnection()) {
+                    Jsonb jsonb = JsonbBuilder.create();
+                    UserDTO user = jsonb.fromJson(request.getReader(), UserDTO.class);
 
-        }catch (JsonbException exp){
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, exp.getMessage());
-        } catch (SQLException exp) {
-            throw new RuntimeException("Failed to obtain a connection",exp);
+                    if (user == null) {
+                        throw new JsonbException("Invalid user");
+                    } else if (user.getUserId() == null || user.getUserId().trim().isEmpty()) {
+                        throw new JsonbException("User id can't be empty");
+                    } else if (user.getUserId().trim().length() < 5) {
+                        throw new JsonbException("User id should be at least 5 chars long");
+                    } else if (user.getName() == null || user.getName().trim().isEmpty()) {
+                        throw new JsonbException("User's name can't be empty");
+                    } else if (!user.getName().trim().matches("[A-Za-z ]+")) {
+                        throw new JsonbException("Invalid name");
+                    } else if (user.getPassword() == null || user.getPassword().trim().isEmpty()) {
+                        throw new JsonbException("Password can't be empty");
+                    } else if (user.getPassword().trim().length() < 5) {
+                        throw new JsonbException("Password should be at lest 5 chars long");
+                    } else if (user.getUserId().contains(":")) {
+                        throw new JsonbException("User id can't contain colon");
+                    }
+
+                    UserService userService = new UserService(connection);
+                    userService.saveUser(user);
+                    response.setStatus(HttpServletResponse.SC_CREATED);
+
+                } catch (JsonbException exp) {
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST, exp.getMessage());
+                } catch (SQLException exp) {
+                    throw new RuntimeException("Failed to obtain a connection", exp);
+                }
+                break;
+
+
+            case "/login":
+                if (request.getContentType() == null || !request.getContentType().startsWith("application/x-www-form-urlencoded")) {
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid content type");
+                    return;
+                }
+
+                String userId = request.getParameter("userId");
+                String password = request.getParameter("password");
+
+                if (userId == null || userId.trim().isEmpty()) {
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST, "User id can't be empty");
+                } else if (password == null || password.trim().isEmpty()) {
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Password can't be empty");
+                } else {
+
+                    try (Connection connection = dataSource.getConnection()) {
+                        UserService userService = new UserService(connection);
+                        UserDTO authenticatedUser = userService.authenticate(userId, password);
+
+                        if (authenticatedUser == null) {
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid login credentials");
+                        } else {
+                            response.setContentType("application/json");
+                            response.getWriter().println(JsonbBuilder.create().toJson(authenticatedUser));
+                        }
+
+                    } catch (SQLException exp) {
+                        throw new RuntimeException("Failed to obtain a connection", exp);
+                    }
+
+                }
         }
 
     }
